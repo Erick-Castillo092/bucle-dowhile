4c6c7629b19b614c95590b30277d029ffd755e29
Erick-Castillo092 <erickelsueco1@gmail.com>
2025-09-10 10:23:11 -0500

    Create script.js
    Se anexa carpeta js con coso script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var intento;
+var numerosecreto = 42;
+
+do {
+    intento = parseInt(prompt("Adivina el numero "));
+    if (intento < numerosecreto)(
+        alert ("El numero es mas grande");
+     ) 
+     if (intento > numerosecreto)(
+        alert ("Es un numero mas pequeño");
+    )
+}while (intento !== numerosecreto);
+
+  
